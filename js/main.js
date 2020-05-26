fffa56322020844b365814e29c9f6bf4ee9a6bf8
Joao Erick <jerick1415@outlook.com>
2020-05-26 20:44:16 -0300

    Adicionando mais aulas

--- js/main.js
+++ js/main.js
@@ -66,3 +66,65 @@ console.log(frase.toLowerCase()); //Todas as letras minúsculas
 //alert(d.getHours());
 //alert(d.getMinutes());
 */
+
+/*------- AULA 004 
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome);
+}
+
+//var validar; //variavel fora da função
+//function validaIdade(idade){
+//    var validar; //variavel local | Se apagar essa linha, será o usado a variavel fora da função (global)
+//    if(idade >= 18){
+//       validar = true;
+//    }
+//    else{
+//        validar = false;
+//    }
+//    return validar;
+//}
+
+//var idade = prompt("Qual a sua idade?");
+//console.log(validaIdade(idade));
+
+alert(soma(5, 10));
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
+*/
+
+/*------- AULA 005 
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar!</b>";
+    //document.getElementById("agradecimento").innerHTML = "Obrigado por clicar!"; //innerHTML coloca o que está entre aspas dentro da tag
+    //document.getElementById("agradecimento");
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado por clicar!");
+} 
+
+function redirecionar(){
+    //window.open("https://www.youtube.com"); //Abre outra aba
+    //window.location.href = "https://www.youtube.com" //Abre na própria aba
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse aqui!";
+    //alert("trocar texto");
+    elemento.innerHTML = "Obrigado por passar o mouse aqui!";
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui"
+}
+
+function load(){
+    alert("Página carregada");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
+*/
